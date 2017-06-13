5cb651978606e171a82e2b48e2b8ccf0a41afacb
zhangpeng <zhangpeng@gendock.com>
2017-06-13 11:56:23 +0800

    ADD: Data Reverse.py

---
+++ Data Reverse.py
@@ -0,0 +1,25 @@
+def data_reverse(data):
+    stack = []
+    for i in range(0, len(data), 8):
+        stack.append(data[i:i + 8])
+    res = []
+    for i in range(len(stack)):
+        res.extend(stack.pop())
+    return res
+
+
+def data_reverse2(data):
+    return [b for a in xrange(len(data) - 8, -1, -8) for b in data[a:a + 8]]
+
+
+def data_reverse3(data):
+    res = []
+    for i in range(len(data) - 8, -1, -8):
+        res.extend(data[i:i + 8])
+    return res
+
+
+if __name__ == '__main__':
+    assert data_reverse( [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]) == [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert data_reverse2( [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]) == [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+    assert data_reverse3( [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]) == [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
